078a87be8c28f239feecae20dd662148c2f4ec31
Sandeepa Kularatne <95904319+SanaIsBestPerson@users.noreply.github.com>
2023-08-14 00:14:34 +0530

    add search facilities

--- src/HarvestApp.java
+++ src/HarvestApp.java
@@ -3,10 +3,12 @@ import javax.swing.JFrame;
 public class HarvestApp{
 
     public static void main(String[] args) {
-        
+
+        //main class
         EmployeeUi empMgr = new EmployeeUi();
         empMgr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         empMgr.setVisible(true);
+
     }
     
 }
